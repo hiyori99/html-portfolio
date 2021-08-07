6bd2211e860aef1cee9a9540b4bdfd75843e1612
hiyori99 <82105285+hiyori99@users.noreply.github.com>
2021-08-07 23:03:12 +0900

    update files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 'use strict';
+$(function() {
+  var $win = $(window),
+      $main = $('main'),
+      $nav = $('nav'),
+      navHeight = $nav.outerHeight(),
+      navPos = $nav.offset().top,
+      fixedClass = 'nav';
 
+  $win.on('load scroll', function() {
+    var value = $(this).scrollTop();
+    if ( value > navPos ) {
+      $nav.addClass(fixedClass);
+      $main.css('margin-top', navHeight);
+    } else {
+      $nav.removeClass(fixedClass);
+      $main.css('margin-top', '0');
+    }
+  });
+});
 {
   const open = document.getElementById('open2');
   const close = document.getElementById('close2');
