dfe5a8aea6049190c4df41d7b39fa3e11040854f
hiyori99 <82105285+hiyori99@users.noreply.github.com>
2021-08-18 00:32:16 +0900

    update files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,50 @@
 'use strict';
 
 {
+  const open = document.getElementById('open1');
+  const close = document.getElementById('close1');
+  const modal = document.getElementById('modal1');
+  const mask = document.getElementById('mask1');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    close.click();
+  });
+}
+{
   const open = document.getElementById('open2');
   const close = document.getElementById('close2');
   const modal = document.getElementById('modal2');
   const mask = document.getElementById('mask2');
+
+  open.addEventListener('click', () => {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+
+  close.addEventListener('click', () => {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+
+  mask.addEventListener('click', () => {
+    close.click();
+  });
+}
+{
+  const open = document.getElementById('open3');
+  const close = document.getElementById('close3');
+  const modal = document.getElementById('modal3');
+  const mask = document.getElementById('mask3');
 
   open.addEventListener('click', () => {
     modal.classList.remove('hidden');
